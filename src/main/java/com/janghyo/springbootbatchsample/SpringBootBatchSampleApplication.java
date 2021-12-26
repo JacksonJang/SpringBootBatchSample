e248940eeb542fb75d34ecac93e71598bbfa4799
장효원 <hyo961015@naver.com>
2021-12-26 21:07:42 +0900

    init

---
+++ src/main/java/com/janghyo/springbootbatchsample/SpringBootBatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.janghyo.springbootbatchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBatchSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janghyo/springbootbatchsample/SpringBootBatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janghyo.springbootbatchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
